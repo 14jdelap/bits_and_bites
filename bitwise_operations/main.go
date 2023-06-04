c2c95a006895ac408c443be2864c0aa3666cf011
José Agustín de la Puente <14jdelap@gmail.com>
2023-06-04 22:45:20 +0100

    Add logs and edit isAnyBitOn argument

--- bitwise_operations/main.go
+++ bitwise_operations/main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"log"
 	"unsafe"
 )
 
 func main() {
+	var message string
 	littleEndian := getEndian()
-	isAnyBitOn(10, littleEndian)
+	if littleEndian {
+		message = "little endian"
+	} else {
+		message = "big endian"
+	}
+	log.Printf("Your computer uses %s byte ordering\n", message)
+
+	var i int32 = 10
+	if isAnyBitOn(i) {
+		message = "has at least one bit on"
+	} else {
+		message = "has no bits on"
+	}
+
+	log.Printf("The integer %d %s\n", i, message)
 }
 
 // isAnyBitOn returns true if at least one bit in the integer is on.
-func isAnyBitOn(i int32, littleEndian bool) bool {
+func isAnyBitOn(i int32) bool {
 	iup := unsafe.Pointer(&i)
 	for i := 0; i < 4; i++ {
 		bitOn := *(*byte)(unsafe.Add(iup, i)) | 0b00000000
